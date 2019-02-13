706e63539034139573e0ae16efa210f28b7cd981
TaimoorSaeed <taimoor.dev@gmail.com>
2019-02-14 02:02:18 +0500

    Completed Day1

---
+++ Day1/Day1Practise.playground/Contents.swift
@@ -0,0 +1,26 @@
+import UIKit
+
+var str = "Hello, playground"
+
+
+var age  = 38
+
+age = -38
+
+var str1 = """
+This goes
+over multiple l
+ines
+"""
+
+var str2 = """
+This goes \
+over multiple \
+lines
+"""
+
+
+var score = 85
+var strN = "Your score was \(score)"
+var results = "You resuls are here \(strN)"
+
